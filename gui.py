6c207fef0f4686aa902354cb058bb371cf95abaf
Nicholas Fantino-Dyer <89417141+nfantinodyer@users.noreply.github.com>
2022-04-25 20:22:57 -0700

    Changed Graphics
    Moved around the boxes. Removed ability to alter output in text_box and ans entry

--- gui.py
+++ gui.py
@@ -8,15 +8,18 @@ window.title("Word Finder")
 window.columnconfigure(0, weight=1,minsize=250)
 window.rowconfigure(0, weight=1, minsize=250)
 
-frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
-frm_form.pack()
 
-text_box = tk.Text(width=50, height=10)
-ans = tk.Entry()
+
+text_box = tk.Text(width=10, height=10)
+ans = tk.Entry(width=11)
 
 def clear():
+    ans.config(state='normal')
+    text_box.config(state='normal')
     text_box.delete(1.0, tk.END)
     ans.delete(0, tk.END)
+    ans.config(state='disabled')
+    text_box.config(state='disabled')
 
 
 def check(length, one, two, three, four, five, reject, cword):
@@ -72,11 +75,17 @@ def check(length, one, two, three, four, five, reject, cword):
         temp=[]
     
     x=len(words)
+    ans.config(state='normal')
     ans.insert(0,str(x)+" results")
     print("The following "+str(x)+" words are "+str(length)+" letters long and contain the letters you entered: ")
+    text_box.config(state='normal')
     for item in words:
         text_box.insert(tk.END, item+"\n")
-    ans.pack()
+    text_box.config(state='disabled')
+
+    
+    ans.pack(side = tk.BOTTOM, padx = 10, pady = 5)
+    ans.config(state='disabled')
 
 
 #llabel = tk.Label(text="Enter the length of the word you want to find: ")
@@ -88,44 +97,71 @@ def check(length, one, two, three, four, five, reject, cword):
 
 
 entrylabel = tk.Label(text="Enter the letters in the correct spots: ")
-entrylabel.pack(side = tk.LEFT)
+entrylabel.pack(side=tk.TOP,anchor=tk.W)
 
-one = tk.Entry(width=3)
-one.pack(side = tk.LEFT)
+frame1 = tk.Frame(window, width=50, relief=tk.SUNKEN)
+frame1.pack(fill=tk.X)
 
-two = tk.Entry(width=3)
-two.pack(side = tk.LEFT)
+one = tk.Entry(frame1,width=3)
+one.pack(side = tk.LEFT,padx=2,pady=5)
 
-three = tk.Entry(width=3)
-three.pack(side = tk.LEFT)
+two = tk.Entry(frame1,width=3)
+two.pack(side = tk.LEFT,padx=2,pady=5)
 
-four = tk.Entry(width=3)
-four.pack(side = tk.LEFT)
+three = tk.Entry(frame1,width=3)
+three.pack(side = tk.LEFT,padx=2,pady=5)
 
-five = tk.Entry(width=3)
-five.pack(side = tk.LEFT)
+four = tk.Entry(frame1,width=3)
+four.pack(side = tk.LEFT,padx=2,pady=5)
+
+five = tk.Entry(frame1,width=3)
+five.pack(side = tk.LEFT,padx=2,pady=5)
+
 
 entrylabel = tk.Label(text="Enter the known letters: ")
-entrylabel.pack()
+entrylabel.pack(anchor=tk.W)
 
-entryword = tk.Entry(width=50)
-entryword.pack()
+entryword = tk.Entry(width=20)
+entryword.pack(anchor=tk.W,padx=2,pady=5)
 
 rejectl = tk.Label(text="Enter the rejected letters: ")
-rejectl.pack()
+rejectl.pack(anchor=tk.W)
 
-reject = tk.Entry(width=50)
-reject.pack()
+reject = tk.Entry(width=20)
+reject.pack(anchor=tk.W,pady=5,padx=2)
 
+
+one.delete(1, tk.END)
+two.delete(1, tk.END)
+three.delete(1, tk.END)
+four.delete(1, tk.END)
+five.delete(1, tk.END)
 
 #int(l.get()) instead of 5
 button = tk.Button(text="Search", width=10, height=2, command=lambda: check(5,one.get(), two.get(), three.get(), four.get(), five.get(),reject.get(), entryword.get()))
-button.pack()
+button.pack(anchor=tk.W,padx=2)
 
-text_box.pack()
+text_box.pack(side=tk.LEFT,padx=2,pady=5)
+text_box.config(state='disabled')
+
+def reset():
+    clear()
+    reject.delete(0, tk.END)
+    entryword.delete(0, tk.END)
+    one.delete(0, tk.END)
+    two.delete(0, tk.END)
+    three.delete(0, tk.END)
+    four.delete(0, tk.END)
+    five.delete(0, tk.END)
+
 
 button = tk.Button(text="Exit", command=quit, width=10, height=2)
-button.pack()
+button.pack(side = tk.BOTTOM, padx=5, pady=5)
+
+reset = tk.Button(text="Clear", width=10, height=2, command=reset)
+reset.pack(side = tk.BOTTOM, padx=5, pady=5)
+
+
 
 
 
